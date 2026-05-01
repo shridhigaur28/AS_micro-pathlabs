61a2f49a36861ac4a303cd4059274d8c1029f4d0
Shreshtha soni <flutekeshavsoni@gmail.com>
2026-05-01 05:25:21 +0000

    Add devcontainer auto setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,12 @@ import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+
+
 export const metadata = {
+   icons: {
+    icon: "/icon.png",
+  },
   title: 'AS MICRO & PATH LABS | Trusted Pathology Lab',
   description: 'Fast, accurate and affordable pathology tests with home sample collection and digital reports. Book your appointment today.',
 };
